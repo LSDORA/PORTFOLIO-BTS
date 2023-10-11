23912e9b2657bf9789a77420453925dfcda47319
Aymerick <120599499+LSDORA@users.noreply.github.com>
2023-10-11 09:42:03 +0200

    INSTALLATION REACT

--- JS/anime.js
+++ JS/anime.js
@@ -1,26 +1,19 @@
-// Créez une scène
-const scene = new THREE.Scene();
+// Fonction pour basculer les propriétés CSS des boutons
+function switchButtons() {
+    const worButton = document.getElementById('wor');
+    const fworButton = document.getElementById('fwor');
+    const ON = document.getElementById('onwor');
+    const OFF = document.getElementById('offwor');
+  
+   
 
-// Créez une caméra
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
 
-// Créez un rendu WebGL
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('scene-container').appendChild(renderer.domElement);
-
-// Chargez le modèle 3D GLTF
-const loader = new THREE.GLTFLoader();
-loader.load('chemin_vers_votre_modele.gltf', (gltf) => {
-    const model = gltf.scene;
-    scene.add(model);
-});
-
-// Animation (facultatif)
-const animate = () => {
-    requestAnimationFrame(animate);
-    // Ajoutez ici votre logique d'animation si nécessaire
-    renderer.render(scene, camera);
-};
-animate();
+    const tempClassList = worButton.classList;
+    worButton.className = fworButton.className;
+    fworButton.className = tempClassList;
+  
+ 
+  
+   
+  }
+  
